90ac557667de2f3aba5dbb63c557558d02c50a67
krina1902 <krinaantala33@gmail.com>
2023-08-27 11:00:45 +0530

    dj

--- mysite/urls.py
+++ mysite/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('social-auth/', include('social_django.urls', namespace='social')), 
-    path('',views.home,name='home'), # <-- here
+    path('',views.home,name='home'),
 ]
